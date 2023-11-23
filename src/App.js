808a10fd481b53e14a53ded12ae459cb9b301da8
Mrinii <melmrini02@gmail.com>
2023-11-23 18:05:23 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Tp5 from './Tp5';
 import CalcState from './CalcState';
 import Slider from './Slider';
@@ -8,24 +7,21 @@ import Formtasks from './Formtasks';
 import UseEffectTp from './UseEffectTp';
 import ThemeContextProvider from './ThemeContext';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    
-      <div className="App">
-        <ThemeContextProvider>
-          <Tp5 />
-        </ThemeContextProvider>
-        <Routes>
-          <Route path="/CalcState" element={<CalcState />} />
-          <Route path="/Slider" element={<Slider />} />
-          <Route path="/Formtasks" element={<Formtasks />} />
-          <Route path="/UseEffectTp" element={<UseEffectTp />} />
-        </Routes>
-        
-      </div>
-    
+    <div className="App">
+      <ThemeContextProvider>
+        <Tp5 />
+      </ThemeContextProvider>
+      <Routes>
+        <Route path="/CalcState" element={<CalcState />} />
+        <Route path="/Slider" element={<Slider />} />
+        <Route path="/Formtasks" element={<Formtasks />} />
+        <Route path="/UseEffectTp" element={<UseEffectTp />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // basename='CC1_react'
-  <BrowserRouter >
+  <BrowserRouter basename='CC1_react'>
     <App />
   </BrowserRouter>
 );
